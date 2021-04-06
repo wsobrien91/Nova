15088d765a01952194487f9d102fe3596e5b680e
Drew <bridgmand@gmail.com>
2021-04-06 15:09:51 -0400

    added api routes

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const connectDB = require('./config/db');
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (re, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 // Start the API server
 app.listen(PORT, function () {
