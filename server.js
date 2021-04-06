48d8f060353f131ef479769a031de533cf82e457
Eamon McHugh <eamonmchugh2@gmail.com>
2021-04-06 15:09:55 -0400

    api routes

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 // Requiring necessary npm packages
 const express = require('express');
 const connectDB = require('./config/db');
-const PORT = process.env.PORT || 8080;
 const app = express();
 
+//Connect DB
+connectDB();
+
 app.get('/', (re, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/post', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 8080;
 
 // Start the API server
 app.listen(PORT, function () {
